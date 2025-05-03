cb01c9965035b953c05e77fb8d425bf97f19527c
Eyad El-Taher <eltahereyad0@gmail.com>
2025-05-04 01:22:53 +0300

    Implement loader

--- src/core/Loader.java
+++ src/core/Loader.java
@@ -1,11 +1,47 @@
 package core;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Loader {
+    
+
     Loader() {
         System.out.println("Loader initialized");
     }
+ 
+    public byte [] loadRom(String filePath) {
 
-    public void load(String romPath, Memory memory) {
-        System.out.println("Loading rom to the memory...");
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            byte [] romData = fis.readAllBytes();
+            validateROM (romData);
+            fis.close();
+            return romData;        
+        } catch (IOException |IllegalArgumentException e) {
+            System.out.println("Error loading ROM: " + e.getMessage());
+            return null;                                                   //take care of the null ya 3mda
+        }
     }
+
+    public boolean validateROM (byte [] data) {
+        
+        if ( data== null || data.length==0) {
+            throw new IllegalArgumentException("ROM file is empty");
+        }
+
+        if (data.length > (0xFFF-0x200)) {
+            throw new IllegalArgumentException("Rom file is larger than the Maximum size (0xFFF-0x200 bytes)");
+        }
+
+        return true;
+    }    
+
+    public List<String> listAvailableROMs() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
 }
+
