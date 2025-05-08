df279bb6675341b86f13b12ffa251b5ae94ee3ab
Eyad Eltaher <eltahereyad0@gmail.com>
2025-05-08 18:42:15 +0300

    Update StoreRegistersInstruction.java

--- src/core/instruction/instructions/StoreRegistersInstruction.java
+++ src/core/instruction/instructions/StoreRegistersInstruction.java
@@ -13,7 +13,7 @@ public class StoreRegistersInstruction extends Instruction {
     public void execute() {
         for (int i = 0; i <= lastRegister; i++) {
             byte value = cpu.getRegisters().getRegister(i);
-            cpu.getMemory().write(cpu.getI(), (char) value);
+            cpu.getMemory().write((char)(cpu.getI()+i),(char)value);
         }
     }
 
