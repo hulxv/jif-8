325dc26965739232f4b3518eea00f101388a9f71
MMRclear <mohamedmahmued128@gmail.com>
2025-05-11 01:14:41 +0300

    add welcome scene

---
+++ src/scenes/welcomeScene.java
@@ -0,0 +1,113 @@
+package core;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class welcomeScene extends Application {
+
+
+    private Font labelFont;
+    private Font label2Font;
+    private Font buttonFont;
+    private Scene scene1;
+    private Stage stage;
+
+
+    @Override
+    public void start(Stage Primarystage) {
+        stage = Primarystage;
+
+
+        labelFont = Font.loadFont(getClass().getResource("/fonts/PressStart2P-Regular.ttf").toExternalForm(), 30);
+        label2Font = Font.loadFont(getClass().getResource("/fonts/PressStart2P-Regular.ttf").toExternalForm(), 40);
+        buttonFont = Font.loadFont(getClass().getResource("/fonts/PressStart2P-Regular.ttf").toExternalForm(), 14);
+
+
+        scene1 = createScene1();
+        stage.setTitle("Game Screen");
+        stage.setScene(scene1);
+        stage.show();
+
+
+    }
+    private Label buildlabel() {
+
+        Label welcome = new Label("Welcome to ");
+        welcome.setFont(labelFont);
+        welcome.setTextFill(Color.web("#24e804"));
+        welcome.setLayoutX(360);
+        welcome.setLayoutY(200);
+        welcome.setPrefSize(600, 50);
+
+        return welcome;
+    }
+
+    private Label buildlabel2() {
+
+        Label jif = new Label("JIF-8 ");
+        jif.setFont(label2Font);
+        jif.setTextFill(Color.web("#24e804"));
+        jif.setLayoutX(400);
+        jif.setLayoutY(270);
+        jif.setPrefSize(700, 100);
+
+        return jif;
+    }
+
+    private Button buildbutton() {
+
+        Button load = new Button("LOAD GAME");
+        load.setFont(buttonFont);
+        load.setLayoutX(430);
+        load.setLayoutY(500);
+        load.setOnAction(event -> {
+            try {
+                filechooser();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return load;
+    }
+
+    private Scene createScene1() {
+        Label welcome = buildlabel();
+        Label jif = buildlabel2();
+        Button load = buildbutton();
+        AnchorPane gameScreen = new AnchorPane();
+        gameScreen.setStyle("-fx-background-color: rgba(33,32,32,1);");
+
+        gameScreen.getChildren().addAll(welcome, jif, load);
+
+        Scene scene1 = new Scene(gameScreen, 1000, 750);
+
+        return scene1;
+
+    }
+
+    private File filechooser() throws IOException {
+
+        FileChooser fc = new FileChooser();
+
+        File file = fc.showOpenDialog(stage);
+
+        Loader loader = new Loader();
+        loader.loadRom(file.getAbsolutePath());
+
+        return file;
+        }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
