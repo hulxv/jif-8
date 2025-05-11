07a64485dd3eca76b61794a7b2305124a4283c5e
mostafa <mstfahmd772005@gmail.com>
2025-05-12 01:01:54 +0300

    Implementing The Sprite and Font instructions for Graphics Operations

--- src/core/instruction/instructions/DrawSpriteInstruction.java
+++ src/core/instruction/instructions/DrawSpriteInstruction.java
@@ -1,11 +1,12 @@
 package core.instruction.instructions;
 
+import core.Display;
 import core.instruction.Instruction;
 
 public class DrawSpriteInstruction extends Instruction {
-    private final int registerX;
-    private final int registerY;
-    private final int height;
+    private final int registerX; // Holds X-Coordinate (VX)
+    private final int registerY; // Holds Y-Coordinate (VY)
+    private final int height; // Number of bytes in the sprite, representing height (each byte is one row of 8 pixels)
 
     public DrawSpriteInstruction(int rx, int ry, int height) {
         this.registerX = rx;
@@ -15,8 +16,28 @@ public class DrawSpriteInstruction extends Instruction {
 
     @Override
     public void execute() {
-        // Implementation will draw sprite at location (VX,VY) with height N
-        System.out.printf("DRAW V%X, V%X, %X\n", registerX, registerY, height);
+        cpu.getRegisters().setRegister(0xF, (byte) 0); // Reset Collision Flag
+        final int pixelWidth = 8; // Sprites are always 8 pixels wide
+
+        int xCoordinate = cpu.getRegisters().getRegister(registerX) & 0xFF;
+        int yCoordinate = cpu.getRegisters().getRegister(registerY) & 0xFF;
+
+        for(int row = 0 ; row < height ; row++) {
+            int spriteByte = cpu.getMemory().read((char) (cpu.getI() + row)); // (cpu.getI() + row) is address
+
+            for (int column = 0 ; column < pixelWidth ; column++) {
+                if((spriteByte & (0x80 >> column)) != 0) {
+                int x = (column + xCoordinate) % Display.WIDTH;
+                int y = (row + yCoordinate) % Display.HEIGHT;
+                boolean currentPixel = cpu.getDisplay().getPixel(x, y);
+
+                if(currentPixel) {
+                    cpu.getRegisters().setRegister(0xF, (byte) 1); // Set collision flag
+                }
+                cpu.getDisplay().setPixel(x, y, currentPixel ^ true);
+                }
+            }
+        }
     }
 
     @Override
--- src/core/instruction/instructions/LoadFontInstruction.java
+++ src/core/instruction/instructions/LoadFontInstruction.java
@@ -3,7 +3,7 @@ package core.instruction.instructions;
 import core.instruction.Instruction;
 
 public class LoadFontInstruction extends Instruction {
-    private final int register;
+    private final int register; // Register Index
 
     public LoadFontInstruction(int register) {
         this.register = register;
@@ -11,8 +11,8 @@ public class LoadFontInstruction extends Instruction {
 
     @Override
     public void execute() {
-        // Implementation will set I to location of sprite for hex digit VX
-        System.out.printf("FONT V%X\n", register);
+        int digit = cpu.getRegisters().getRegister(register) & 0x0F; // Get lower 4 bits (0–F)
+        cpu.setI((char) (0x50 + (digit * 5))); // Set I to font sprite address
     }
 
     @Override
