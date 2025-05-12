0cc3cfd301e7c3f16ea378ee6bcb352d1eee8b4c
hulxv <hulxxv@gmail.com>
2025-05-13 01:48:00 +0300

    refactor: improve `Display` and add `drawFlag`

--- src/core/Display.java
+++ src/core/Display.java
@@ -1,31 +1,42 @@
 package core;
 
+import java.util.Arrays;
+
 public class Display {
 
     private boolean[][] pixels;
     public static final int WIDTH = 64;
     public static final int HEIGHT = 32;
-    
+    public boolean drawFlag = false;
+
     public Display() {
+        reset();
+    }
+
+    public boolean getDrawFlag() {
+        return drawFlag;
+    }
+
+    public void setDrawFlag() {
+        drawFlag = true;
+    }
+
+    public void clearDrawFlag() {
+        drawFlag = false;
+    }
+
+    public void reset() {
         pixels = new boolean[WIDTH][HEIGHT];
     }
 
-    public void clear() {
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
-                pixels[i][j] = false;
-            }
-        }
-    }
-
     public void setPixel(int x, int y, boolean value) {
-        if (x < WIDTH && x >= 0 && y < HEIGHT && y >=0) {
+        if (x < WIDTH && x >= 0 && y < HEIGHT && y >= 0) {
             pixels[x][y] = value;
         }
     }
 
     public boolean getPixel(int x, int y) {
-        if (x < WIDTH && x >= 0 && y < HEIGHT && y >=0) {
+        if (x < WIDTH && x >= 0 && y < HEIGHT && y >= 0) {
             return pixels[x][y];
         }
         throw new IndexOutOfBoundsException("Invalid Pixle Coordinates");
